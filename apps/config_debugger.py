eb3a2418f8ed281de2741f3c1d152b60dbcc4cfb
Naheev Tinker <naheev@hotmail.co.nz>
2013-08-18 09:01:50 -0600

    Convert streams app into package

---
+++ apps/config_debugger.py
@@ -0,0 +1,21 @@
+"""
+Debug
+=====
+
+Loads and dsiplay config info that is used by running app.
+"""
+from bottle import Bottle, request
+import bottle
+
+
+def create_app(custom_config=None, host_app=None):
+    app = host_app if host_app else Bottle()
+
+    if custom_config:
+        app.config.update(custom_config)
+
+    @app.route('/')
+    def index():
+        return app.config
+
+    return app
