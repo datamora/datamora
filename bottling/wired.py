49ac9239bd440e06474f35dbb07b4c0be24e4751
Naheev Tinker <naheev@hotmail.co.nz>
2013-09-25 23:35:55 -0600

    More work on the wired specs

--- bottling/wired.py
+++ bottling/wired.py
@@ -3,3 +3,11 @@ class Container(object):
     """Holds the context for a wire spec"""
     def __init__(self, spec):
         self.spec = spec
+
+    def _is_simple_spec(self, spec):
+        return type(spec) is str
+
+    def provide(self, handle):
+        the_spec = self.spec[handle]
+        if self._is_simple_spec(the_spec):
+            return eval(the_spec)
--- spec/bottling_specs/wired_specs.py
+++ spec/bottling_specs/wired_specs.py
@@ -7,3 +7,18 @@ class describe_Container:
         c = Container(spec)
 
         assert c.spec == spec
+
+class describe_Container_given_simple_spec_for_function_call:
+    def it_returns_the_result_of_a_function_call_when_requested(self):
+        spec = {
+            'a_thing': 'thing_factory_function()'
+        }
+        c = Container(spec)
+
+        thing = c.provide('a_thing')
+
+        assert thing['name'] == 'Spkolorak'
+
+
+def thing_factory_function():
+    return {'name':'A thing!'}
