bf06420e45417e091e6402d1d2f41d16fef39361
Naheev Tinker <naheev@hotmail.co.nz>
2013-10-03 00:08:30 -0600

    At a reasonably working state for wire-specs.

--- bottling/wired.py
+++ bottling/wired.py
@@ -1,13 +1,75 @@
+import sys
+
 
 class Container(object):
-    """Holds the context for a wire spec"""
-    def __init__(self, spec):
-        self.spec = spec
+    """Accepts a specification for wiring up components and materializes that spec 
+    as a context"""
+    def __init__(self, spec, loader=None):
+        self._spec = spec
+        self._load = loader if loader else load
+        self._context = {}
 
-    def _is_simple_spec(self, spec):
-        return type(spec) is str
+    def register(self, handle, component):
+        self._context[handle] = component
+        return component
 
-    def provide(self, handle):
-        the_spec = self.spec[handle]
-        if self._is_simple_spec(the_spec):
-            return eval(the_spec)
+    def get(self, handle):
+        if self._context.has_key(handle):
+            return self._context[handle]
+        return self.register(handle, self.resolve(handle))
+
+    def resolve(self, handle):
+        if not self._spec.has_key(handle):
+            raise UnknownHandleError()
+
+        definition = self._spec[handle]['create']
+        source = definition['source']
+        args = definition.get('args', {})
+        return self.load(source, args)
+
+    def load(self, source, args=None):
+        if args is None:
+            return self._load(source)
+        return self._load(source, **args)
+
+
+        
+
+
+class UnknownHandleError(Exception):
+    pass
+
+
+def load(target, **namespace):
+    """ Import a module or fetch an object from a module.
+
+        * ``package.module`` returns `module` as a module object.
+        * ``pack.mod:name`` returns the module variable `name` from `pack.mod`.
+        * ``pack.mod:func()`` calls `pack.mod.func()` and returns the result.
+
+        The last form accepts not only function calls, but any type of
+        expression. Keyword arguments passed to this function are available as
+        local variables. Example: ``import_string('re:compile(x)', x='[a-z]')``
+
+        NOTE: Ripped straight from bottle.py!
+    """
+    module, target = target.split(":", 1) if ':' in target else (target, None)
+    if module not in sys.modules: __import__(module)
+    if not target: return sys.modules[module]
+    if target.isalnum(): return getattr(sys.modules[module], target)
+    package_name = module.split('.')[0]
+    namespace[package_name] = sys.modules[package_name]
+    return eval('%s.%s' % (module, target), namespace)
+
+
+# wire({
+#     'datastores.master.engine': {
+#         'create': {
+#             'from': 'sqlalchemy:create_engine(url, echo=echo)',
+#             'args': {
+#                 'url': 'sqlite:///:memory:',
+#                 'echo': False
+#             }
+#         }
+#     }
+# })
--- spec/bottling_specs/wired_specs.py
+++ spec/bottling_specs/wired_specs.py
@@ -1,15 +1,19 @@
-from bottling.wired import Container
+import fudge
+from bottling.wired import Container, Loader
 
 
 class describe_Container:
+
     def it_initializes_with_a_spec(self):
         spec = {}
         c = Container(spec)
 
         assert c.spec == spec
 
+
 class describe_Container_given_simple_spec_for_function_call:
-    def it_returns_the_result_of_a_function_call_when_requested(self):
+
+    def _returns_the_result_of_a_function_call_when_requested(self):
         spec = {
             'a_thing': 'thing_factory_function()'
         }
@@ -18,6 +22,48 @@ class describe_Container_given_simple_spec_for_function_call:
         thing = c.provide('a_thing')
 
         assert thing['name'] == 'Spkolorak'
+
+
+class describe_Loader:
+
+    @fudge.test
+    def it_uses_a_resolver_to_load_a_reference(self):
+        my_module = 'my_package.my_module'
+        fake_resolver = (fudge.Fake('resolver')
+                                .expects_call()
+                                .with_args(my_module))
+        l = Loader(fake_resolver)
+
+        l.load(my_module)
+
+
+class escribe_load:
+
+    @fudge.patch('bottle.load')
+    def it_loads_a_module_by_reference(self, bottle_load):
+        module_ref = 'my_module'
+        loaded_module = fudge.Fake('module')
+        (bottle_load
+            .expects_call()
+            .with_args(module_ref)
+            .returns(loaded_module))
+
+        #m = load(module_ref)
+
+        assert m is not None
+
+    @fudge.patch('bottle.load')
+    def it_loads_a_variable_from_a_module(self, bottle_load):
+        var_ref = 'my_module:a_var'
+        loaded_module = fudge.Fake('module').provides('a_var')
+        (bottle_load
+            .expects_call()
+            .with_args(var_ref)
+            .returns(loaded_module))
+
+        v = load(var_ref)
+
+        assert v is not None
 
 
 def thing_factory_function():
