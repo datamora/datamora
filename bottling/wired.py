b0a23f7769f9da807b490b9e07bec56d8ef20d5d
Naheev Tinker <naheev@hotmail.co.nz>
2013-10-04 00:22:41 -0600

    Use Specification to handle determining ght right factory to use.

--- bottling/wired.py
+++ bottling/wired.py
@@ -1,28 +1,37 @@
 import sys
 
 
-class Container(object):
-    """Accepts a specification for wiring up components and materializes that spec 
-    as a context"""
+class Context(object):
+    """Accepts a specification for wiring up components and provides methods for 
+    materializing that spec as compnent instances"""
+
     def __init__(self, spec, loader=None):
+        """
+        :param wire_spec: `dict`
+        :param loader: a callable that handles the actual loading/importing of components
+        """
         self._spec = spec
         self._load = loader if loader else load
-        self._context = {}
+        self._components = {}
 
-    def register(self, handle, component):
-        self._context[handle] = component
+    def register(self, ref, component):
+        self._components[ref] = component
         return component
 
-    def get(self, handle):
-        if self._context.has_key(handle):
-            return self._context[handle]
-        return self.register(handle, self.resolve(handle))
+    def get(self, ref):
+        if self._components.has_key(ref):
+            return self._components[ref]
+        return self.register(ref, self.resolve(ref))
 
-    def resolve(self, handle):
-        if not self._spec.has_key(handle):
-            raise UnknownHandleError()
+    def resolve(self, ref):
+        if not self._spec.has_key(ref):
+            raise UnknownDirectiveIdError()
 
-        definition = self._spec[handle]['create']
+        factory = self._spec.get_factory(ref)
+        return factory.create()
+
+        directive = self._spec[ref]
+        definition = directive['create']
         source = definition['source']
         args = definition.get('args', {})
         return self.load(source, args)
@@ -33,10 +42,20 @@ class Container(object):
         return self._load(source, **args)
 
 
-        
+class Specification(object):
+    """Represents a context-scoped specification for wiring up components 
+    and provides the relevant factories for materializing component instances
+    for individual directives within the specification"""
+
+    def __init__(self, data):
+        self._data = data
+
+    def get_factory(self, ref):
+        """Returns the relvant factory for the given directive id"""
+        directive = self._data[ref]
 
 
-class UnknownHandleError(Exception):
+class UnknownDirectiveIdError(Exception):
     pass
 
 
@@ -62,14 +81,16 @@ def load(target, **namespace):
     return eval('%s.%s' % (module, target), namespace)
 
 
-# wire({
-#     'datastores.master.engine': {
-#         'create': {
-#             'from': 'sqlalchemy:create_engine(url, echo=echo)',
-#             'args': {
-#                 'url': 'sqlite:///:memory:',
-#                 'echo': False
-#             }
-#         }
-#     }
-# })
+c4 = Container({
+    hello: 'You have been "Hello"ed!',
+    engine: {
+        'create': {
+            'source': 'sqlalchemy:create_engine(url, echo=echo)',
+            'args': {
+                'url': 'sqlite:///:memory:',
+                'echo': False
+            }
+        }
+    },
+
+})
